893deef188f7a6a539bbfc0a42ac348270d5513e
Brooks <brooks@brookss-macbook-pro.local>
2018-05-31 16:40:23 +0100

    Initial Commit

---
+++ Test CocoaPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test CocoaPods
+//
+//  Created by Brooks on 31/05/2018.
+//  Copyright © 2018 Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
